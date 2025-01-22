34353d6b946eabe823ed2610be96e0d006ab3fa8
jalajat <158625696+jalajat@users.noreply.github.com>
2025-01-22 17:54:15 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -51,7 +51,8 @@ if time_to_insert:
 #New Section to smoothiefroot nutrition infromation
 import requests
 smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
-st.text(smoothiefroot_response.json())
+#st.text(smoothiefroot_response.json())
+sf_df=st.dataframe(data=smoothiefroot_response.json(),use_container_width=true)
    
 
 
